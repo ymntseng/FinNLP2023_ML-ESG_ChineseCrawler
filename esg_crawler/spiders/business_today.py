d4c5e349f65d9e9eedb18a65a5f98f156b2593c8
ymntseng <ymn.tseng@gmail.com>
2023-04-15 01:20:16 +0800

    add: comment out

--- esg_crawler/spiders/business_today.py
+++ esg_crawler/spiders/business_today.py
@@ -9,6 +9,7 @@ class BusinessTodaySpider(scrapy.Spider):
     start_urls = ['http://esg.businesstoday.com.tw/catalog/180686/']
 
     def __init__(self, dataset='Train'):
+        # args: dataset = 'Train' or 'Dev'
         self.data = pd.read_json(f'./data/ML-ESG_Chinese_{dataset}.json')
 
     def parse(self, response):
